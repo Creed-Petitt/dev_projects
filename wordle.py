7cdf0d29176a99c59a80e2c920ee9a491dff1559
Creed Petitt <creedpetitt@gmail.com>
2025-05-11 18:44:00 -0500

    wordle game

--- wordle.py
+++ wordle.py
@@ -7,18 +7,71 @@ def getWord(filename):
         with open(filename, "r") as file:
             words = file.read().splitlines()
             word = random.choice(words)
-            print(word)
+            return word
         
     except:
         print("File was not found")
 
 
 def compareWord(word, guess):
-    pass
+    result = ['X'] * 5
 
+    if word == guess:
+        return "!!!!!"
+       
+
+    for i in range(0, len(word)):
+        if guess[i] == word[i]:
+            result[i] = '!'
+    
+    for i in range(0, len(word)):
+        if guess[i] in word and result[i] != '!':
+            result[i] = '*'
+    res = ''.join(result)   
+    return res
+
+        
 def main():
+   
+    guesses = 0
+    MAX_GUESSES = 5
+    
     getWord(FILE_PATH)
+    word = getWord(FILE_PATH).upper()
 
+    var = 0
+    for i in range(guesses, MAX_GUESSES):
+    
+        print("******************************")
+        print(f"You have {5 - i} guesses remaining")
+        print("------------------------------")
+        
+        while True:
+            print("GUESS A 5 LETTER WORD")
+            print("------------------------------")
+           
+            guess = input().upper()
+            print()
+
+            if(len(guess) == 5 and guess.isalpha()):
+                break
+            else:
+                print("Enter a valid 5 letter word")
+                print("------------------------------")
+        
+
+        print(compareWord(word, guess))
+        
+        if compareWord(word, guess) == '!!!!!':
+            print(f"You guessed correctly! The word was {word}")
+            break
+        
+        
+        print()
+        var += 1
+        if var >= MAX_GUESSES:
+            print(f"You lost! The word was {word}")
+    
 
 
 if __name__ == "__main__":
